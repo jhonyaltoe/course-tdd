322b8a020cb6d3d5158a1b158077935d08a76dee
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-06-14 15:23:57 -0300

    refactor: :recycle: make SingUpController to follow a protocol

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,8 +1,8 @@
-import { type HttpRequest, type HttpResponse } from '../protocols'
+import { type HttpRequest, type HttpResponse, type Controller } from '../protocols'
 import { MissingParamErrors } from '../errors'
 import { badRequest } from '../helpers'
 
-export class SingUpController {
+export class SingUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse | undefined {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
