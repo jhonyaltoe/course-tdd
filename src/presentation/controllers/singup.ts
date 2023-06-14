d1e0b740c7586c2f92331b731336fd2ac02753c9
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-06-14 16:26:30 -0300

    feat: :sparkles: ensure SingUpController if an invalid email is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,14 +1,22 @@
-import { type HttpRequest, type HttpResponse, type Controller } from '../protocols'
-import { MissingParamErrors } from '../errors'
+import { type HttpRequest, type HttpResponse, type Controller, type EmailValidator } from '../protocols'
+import { InvalidParamError, MissingParamError } from '../errors'
 import { badRequest } from '../helpers'
 
 export class SingUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse | undefined {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamErrors(field))
+        return badRequest(new MissingParamError(field))
       }
     }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) return badRequest(new InvalidParamError('email'))
   }
 }
