8dd0fa22fa372c3eb29d4d88a0ac0c4be0aa0975
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-06-14 15:05:08 -0300

    feat: :sparkles: ensure SingUpController returns an error if no password is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -4,7 +4,7 @@ import { badRequest } from '../helpers'
 
 export class SingUpController {
   handle (httpRequest: HttpRequest): HttpResponse | undefined {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamErrors(field))
