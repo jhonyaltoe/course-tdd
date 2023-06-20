aa0589070448ca88a6762ee027e207d7535c393a
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-06-20 14:25:54 -0300

    feat: :sparkles: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/useCases/addAccount/db-addaccount.ts
+++ src/data/useCases/addAccount/db-addaccount.ts
@@ -1,19 +1,22 @@
-import { type AccountModel, type AddAccountModel, type AddAccount, type Encrypter } from './db-addaccount-protocols'
+import { type AccountModel, type AddAccountModel, type AddAccount, type Encrypter, type AddAccountRepository } from './db-addaccount-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+    const hashedPassword = await this.encrypter.encrypt(account.password)
+    await this.addAccountRepository.add(Object.assign({}, account, { password: hashedPassword }))
     return await Promise.resolve({
-      id: 'any-id',
-      name: 'any-name',
-      email: 'any-email@mail.com',
-      password: 'any-password'
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
     })
   }
 }
