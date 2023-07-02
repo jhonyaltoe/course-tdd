494e29d3362738b75269507e1c4dea1d6898c056
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-02 10:28:08 -0300

    test: :white_check_mark: ensure SingUp route return an account on success

---
+++ src/main/routes/singup-routes.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Singup Routes', () => {
+  test('Should return non success', async () => {
+    app.post('/test-cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .post('/test-cors')
+      .send({
+        name: 'Jhony',
+        email: 'jhonymikea@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
+  })
+})
