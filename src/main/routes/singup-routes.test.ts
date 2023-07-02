28d6d95cfb7750ae79ef5584a4a2060971812da5
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-02 11:33:59 -0300

    refactor: :recycle: connect to mongodb before integrations tests

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('Singup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(globalThis.__MONGO_URI__)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return non success', async () => {
     app.post('/test-cors', (req, res) => {
       res.send()
