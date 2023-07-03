45cafbe8b83ff9c4699e1462c89b211eaf96aafb
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-03 16:26:30 -0300

    test: :white_check_mark: esure AccountMongoRepository return correct object without _id key

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -33,5 +33,6 @@ describe('Account Mongo Repository', () => {
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
+    expect(Object.keys(account)).toHaveLength(4)
   })
 })
