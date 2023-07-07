83878549fcdc948415be5f50dc0afcd43f01dd78
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-07 17:24:03 -0300

    feat: :sparkles: add LogMongoRepository to SingUpFactory

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -1,17 +1,19 @@
+import { type Controller } from '../../presentation/protocols'
 import { DbAddAccount } from '../../data/useCases/addAccount/db-addaccount'
 import { BcryptAdapter } from '../../infra/cryptografy/bcrypt-adapter'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { SingUpController } from '../../presentation/controllers/singUp/singup'
 import { EmailValidatorAdapter } from '../../utils/email.validator-adapter'
 import { LogContollerDecorator } from '../decorators/log'
-import { type Controller } from '../../presentation/protocols'
 
 export const makeSingUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
+  const logMongoRepository = new LogMongoRepository()
   const accountMongoRepository = new AccountMongoRepository()
   const bcryptAdapter = new BcryptAdapter(salt)
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const singUpController = new SingUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogContollerDecorator(singUpController)
+  return new LogContollerDecorator(singUpController, logMongoRepository)
 }
