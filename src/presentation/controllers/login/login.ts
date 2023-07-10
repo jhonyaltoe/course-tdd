d3ac450e29f274ffa90f3bbd7a75b534fea01b07
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-10 16:24:23 -0300

    feat: :sparkles: ensure LoginController return 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,7 +1,7 @@
 import { type EmailValidator } from '../singUp/singup-protocols'
 import { type HttpRequest, type HttpResponse, type Controller } from '../../protocols'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers'
+import { badRequest, serverError } from '../../helpers'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -11,20 +11,24 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return badRequest(new MissingParamError('email'))
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return badRequest(new MissingParamError('email'))
+      }
+      if (!password) {
+        return badRequest(new MissingParamError('password'))
+      }
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      return {
+        body: {},
+        statusCode: 0
+      } satisfies HttpResponse
+    } catch (error) {
+      return serverError(error)
     }
-    if (!password) {
-      return badRequest(new MissingParamError('password'))
-    }
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-    return {
-      body: {},
-      statusCode: 0
-    } satisfies HttpResponse
   }
 }
