26ee27e6c54552a77105fd0f78ac6dbdf4a523fb
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-10 15:35:01 -0300

    feat: :sparkles: ensure LoginController calls EmailValidador with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
+import { type EmailValidator } from '../singUp/singup-protocols'
+import { type HttpRequest, type HttpResponse, type Controller } from '../../protocols'
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers'
-import { type HttpRequest, type HttpResponse, type Controller } from '../../protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return badRequest(new MissingParamError('email'))
@@ -10,6 +17,10 @@ export class LoginController implements Controller {
     if (!httpRequest.body.password) {
       return badRequest(new MissingParamError('password'))
     }
-    throw new Error('No return implemented')
+    this.emailValidator.isValid(httpRequest.body.email)
+    return {
+      body: {},
+      statusCode: 0
+    } satisfies HttpResponse
   }
 }
