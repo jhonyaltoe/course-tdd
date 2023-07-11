93f2a97a68e0f699869ac54e5bbc433dcd5625d4
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-11 15:49:43 -0300

    test: :white_check_mark: ensure LoginController returns 500 if Authentication throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -106,4 +106,11 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(unauthorized())
   })
+
+  test('Should return 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockRejectedValue(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toStrictEqual(serverError(new Error()))
+  })
 })
