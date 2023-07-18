03f09ad5c8e1d7c1de1aa334e08be4c92eefe8bc
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-18 13:36:56 -0300

    refactor: :recycle: move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,20 +1,37 @@
 import { type Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | null {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub: Validation = makeValidation()
+  const sut = new ValidationComposite([validationStub, validationStub])
+  return {
+    sut,
+    validationStub
+  }
+}
+
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error | null {
-        return null
-      }
-    }
-    const validationStub: Validation = new ValidationStub()
+    const { sut, validationStub } = makeSut()
+    const validationError = new Error('validation error')
     jest.spyOn(validationStub, 'validate')
       .mockReturnValueOnce(null)
-      .mockReturnValueOnce(new Error('validate error'))
-    const sut = new ValidationComposite([validationStub, validationStub])
+      .mockReturnValueOnce(validationError)
     const error = sut.validate({ field: 'any_value' })
-    expect(error).toEqual(new Error('validate error'))
-    expect(error?.message).toBe('validate error')
+    expect(error).toEqual(validationError)
   })
 })
