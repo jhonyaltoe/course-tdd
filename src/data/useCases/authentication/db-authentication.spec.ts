439626cec5ddca51bcb0f3b2e8fd48d4b4221bc7
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-20 16:55:50 -0300

    test: :white_check_mark: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/useCases/authentication/db-authentication.spec.ts
@@ -0,0 +1,26 @@
+import { type LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+import { type AccountModel } from '../addAccount/db-addaccount-protocols'
+import { DbAuthentication } from './db-authentication'
+
+describe('DbAuthentication UseCase', () => {
+  test('Should call LoadAccountByEmailRepository with correct email', async () => {
+    class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+      async load (email: string): Promise<AccountModel> {
+        return {
+          id: 'any_id',
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        }
+      }
+    }
+    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+    const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
+    await sut.auth({
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+})
