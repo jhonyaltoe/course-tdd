4dd63387ff404a5f36247ca8b4b0f713ff7dd282
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-21 15:12:54 -0300

    test: :white_check_mark: ensure DbAuthentication trhows if HashCompare throws

--- src/data/useCases/authentication/db-authentication.spec.ts
+++ src/data/useCases/authentication/db-authentication.spec.ts
@@ -81,4 +81,13 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFakeAuthentication())
     expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
+
+  test('Should throw if HashCompare throws', async () => {
+    const { sut, hashCompareStub } = makeSut()
+    jest.spyOn(hashCompareStub, 'compare').mockRejectedValue(
+      new Error('HashCompare error')
+    )
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrowError(/HashCompare error$/)
+  })
 })
