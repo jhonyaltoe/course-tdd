ad01ff52b201962dc376d419cd0c2d896b7b091a
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-21 15:44:36 -0300

    feat: :sparkles: esure DbAuthentication calls TokenGenerator with correct id

--- src/data/useCases/authentication/db-authentication.ts
+++ src/data/useCases/authentication/db-authentication.ts
@@ -1,17 +1,19 @@
 import { type AuthenticationModel, type Authentication } from '../../../domain/useCases'
 import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { type HashCompare } from '../../protocols/cryptografy'
+import { type TokenGenerator, type HashCompare } from '../../protocols/cryptografy'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashCompare: HashCompare
+    private readonly hashCompare: HashCompare,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (!account) return null
     await this.hashCompare.compare(authentication.password, account.password)
+    await this.tokenGenerator.generate(account.id)
     return null
   }
 }
