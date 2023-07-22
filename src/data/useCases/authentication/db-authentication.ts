21cbd61e8d3049ca09ee5a13a46ac98b292213d0
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-07-22 18:55:43 -0300

    feat: :sparkles: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/useCases/authentication/db-authentication.ts
+++ src/data/useCases/authentication/db-authentication.ts
@@ -1,12 +1,14 @@
 import { type AuthenticationModel, type Authentication } from '../../../domain/useCases'
 import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 import { type TokenGenerator, type HashCompare } from '../../protocols/cryptografy'
+import { type UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashCompare: HashCompare,
-    private readonly tokenGenerator: TokenGenerator
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
@@ -15,6 +17,7 @@ export class DbAuthentication implements Authentication {
     const isValid = await this.hashCompare.compare(authentication.password, account.password)
     if (!isValid) return null
     const accessToken = await this.tokenGenerator.generate(account.id)
+    await this.updateAccessTokenRepository.update(account.id, accessToken)
     return accessToken
   }
 }
