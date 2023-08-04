ebecab9334745252332852734e30cc96b005b30b
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-08-04 16:19:38 -0300

    refactor: ♻️ rename signup routs to login routs

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '@infra/db/mongodb/helpers/mongo-helper'
 
-describe('Singup Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(globalThis.__MONGO_URI__)
   })
@@ -16,18 +16,20 @@ describe('Singup Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return on success', async () => {
-    app.post('/test-cors', (req, res) => {
-      res.send()
-    })
-    await request(app)
-      .post('/test-cors')
-      .send({
-        name: 'Jhony',
-        email: 'jhonymikealtoe@hotmail.com',
-        password: '123',
-        passwordConfirmation: '123'
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      app.post('/test-cors', (req, res) => {
+        res.send()
       })
-      .expect(200)
+      await request(app)
+        .post('/test-cors')
+        .send({
+          name: 'Jhony',
+          email: 'jhonymikealtoe@hotmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })
